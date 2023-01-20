55d873bc947337d189b843f3223dfa721fab136a
Travis Farmer <travis.farmer@outlook.com>
2023-01-20 14:33:47 -0500

    trying to setup demo

--- modbus_slave_lcnc_changer.ino
+++ modbus_slave_lcnc_changer.ino
@@ -14,6 +14,11 @@
 */
 
 #include <ModbusRTUSlave.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,40,2);
+
 const float Y_Center_Spacing = 3.50;
 const float X_Center_Spacing = 3.50;
 const int Y_Num_Pockets = 10;
@@ -28,9 +33,9 @@ const byte id = 1;
 const unsigned long baud = 9600;
 const unsigned int bufSize = 256;
 
-const unsigned int numCoils = 2;
+const unsigned int numCoils = 3;
 const unsigned int numDiscreteInputs = 2;
-const unsigned int numHoldingRegisters = 2;
+const unsigned int numHoldingRegisters = 3;
 const unsigned int numInputRegisters = 16;
 
 const byte buttonPins[numDiscreteInputs] = {2, 3};
@@ -41,6 +46,25 @@ const byte dePin = 12;
 const byte ledPin = 13;
 const byte potPins[numInputRegisters] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
 
+int gblStatusID = 0;
+int gblToolOut = 0;
+int gblToolFetched = 0;
+int gblErrCode = 0;
+char gblStatTxt[36] = "";
+bool gblStatLcnc = false;
+bool gblToolChange = false;
+bool gblToolPrepare = false;
+bool gblToolIsChanged = false;
+bool gblToolIsPrepared = false;
+bool gblToolFetchIsReturn = false;
+long gblToolNumber = 0;
+long gblToolPrepNumber = 0;
+long gblToolPrepPocket = 0;
+
+
+
+unsigned long lastDispTim = 0UL;
+
 byte buf[bufSize];
 ModbusRTUSlave modbus(Serial1, buf, bufSize, dePin);
 
@@ -48,70 +72,87 @@ byte dutyCycle = 0;
 boolean toneActive = 0;
 unsigned int toneFrequency = 0;
 
-void GoHomeLoc() {
-    Serial2.println("G0 Z0");
+void Disp() {
+    lcd.clear();
     char buffer[40];
-    sprintf(buffer, "G0 X%d Y%d", X_Home_Loc, Y_Home_Loc);
-    Serial2.println(buffer);
+    sprintf(buffer, "Err: %d Tool Out: %d Fetched: %d", gblErrCode, gblToolOut, gblToolFetched);
+    lcd.setCursor(0,0);
+    lcd.print(buffer);
+    char bufferb[40];
+    sprintf(bufferb, "%d: %s", gblStatusID, gblStatTxt);
+    lcd.setCursor(0,1);
+    lcd.print(bufferb);
+
 }
 
-void GoChangeLoc() {
-    Serial2.println("G0 Z0");
-    char buffer[40];
-    sprintf(buffer, "G0 X%d Y%d", X_Change_Loc, Y_Change_Loc);
-    Serial2.println(buffer);
+void setStatTxt(String InStr) {
+  InStr.toCharArray(gblStatTxt, 36);
 }
 
-void GoToolLoc(int ToolNum) {
-    int PocX = 0;
-    int PocY = 0;
-    int total_pockets = (X_Num_Pockets * Y_Num_Pockets);
-    int pockets_per_line = (total_pockets / X_Num_Pockets);
-    for (int i=1; i>X_Num_Pockets; i++) {
-        if ((Y_Num_Pockets*y) > ToolNum && (Y_Num_Pockets*(y-1)) < ToolNum) {
-            PocY = ToolNum;
-            PocX = i;
-            break;
-        }
+void SetupStatus() {
+  if (bitRead(gblStatusID,0) == 1) {
+    digitalWrite(ledPin,HIGH);
+    setStatTxt("LinuxCNC Enabled");
+  } else if (bitRead(gblStatusID,0) == 0) {
+    digitalWrite(ledPin,LOW);
+    setStatTxt("Linuxcnc Disabled");
+  }
+
+}
+
+void lcncMachineOn(bool MacStat) {
+    // what to do when machine is turned on or off
+    bitWrite(gblStatusID,0,MacStat? 1:0); // set status bit 0
+
+
+}
+
+void lcncChangeTool(bool ChngStat) {
+    // what to do when a tool change is called
+    if (gblToolIsPrepared == true && gblToolPrepNumber != gblToolNumber && gblToolFetchIsReturn != true) {
+        // tool preped is not current tool, so continue with change
+
+        String tmpStr = "Change from: ";
+        tmpStr += gblToolNumber;
+        tmpStr += " To: ";
+        tmpStr += gblToolPrepNumber;
+        setStatTxt(tmpStr);
+        gblToolFetched = gblToolNumber;
+        gblToolFetchIsReturn = true;
+        gblToolNumber = gblToolPrepNumber;
+        gblToolOut = gblToolNumber;
+        gblToolIsChanged = true;
+
+    } else {
+        // something is wrong, assert an error
+        bitWrite(gblErrCode,0,1);
+        setStatTxt("Tool Num Mismatch");
     }
-    float PocketX = (X_Center_Spacing * ((float)PocX-1));
-    float PocketY = (Y_Center_Spacing * ((float)PocY-1));
-    Serial2.println("G0 Z0");
-    char buffer[40];
-    sprintf(buffer, "G0 X%d Y%d", PocketX, PocketY);
-    Serial2.println(buffer);
 }
 
-bool PickTool() {
-    // TODO: test if we already have tool
-    // TODO: open jaws, or assert error (return false;)
-    char buffer[40];
-    sprintf(buffer, "G0 Z%d", Z_Tool_Depth);
-    Serial2.println(buffer);
-    // TODO: test if tool there
-    // TODO: close jaws, or assert error (return false;)
-    Serial2.println("G0 Z0");
-    // TODO: test if tool still there
-    // TODO: if not, assert error (return false;)
-    // TODO: else (return true;)
-}
-
-bool PlaceTool() {
-    //TODO: test if we have a tool to place
-    //TODO: or assert error (return false;)
-    //TODO: test if pocket is empty
-    //TODO: or assert error (return false;)
-    char buffer[40];
-    sprintf(buffer, "G0 Z%d", Z_Tool_Depth);
-    Serial2.println(buffer);
-    //TODO: open jaws
-    Serial2.println("G0 Z0");
-    //TODO: do we still have the tool
-    //TODO: if so, assert error (return false;)
-    //TODO: else (return true;)
+void lcncPrepTool(bool PrepStat) {
+    if (gblToolIsPrepared != true) {
+        if (gblToolFetchIsReturn == true) {
+            // return tool first, before prefetch
+            String tmpStr = "Returning: ";
+            tmpStr += gblToolPrepNumber;
+            setStatTxt(tmpStr);
+            gblToolFetchIsReturn = false;
+            gblToolPrepNumber = 0;// return is re-stocked in correct pocket, according to tool number
+        }
+        // prep the next tool
+        String tmpStrB = "Prepped: ";
+        tmpStrB += gblToolPrepNumber;
+        setStatTxt(tmpStrB);
+        gblToolPrepNumber = gblToolPrepNumber; // put in place as temporary
+        gblToolFetched = gblToolPrepNumber;
+        gblToolIsPrepared = true;
+    }
 }
 
 void setup() {
+  lcd.init();                      // initialize the lcd
+  lcd.backlight();
   pinMode(buttonPins[0], INPUT_PULLUP);
   pinMode(buttonPins[1], INPUT_PULLUP);
   pinMode(pwmPin, OUTPUT);
@@ -131,11 +172,12 @@ void setup() {
 
 void loop() {
   modbus.poll();
-  if (toneActive == true and toneFrequency > 0) {
-    tone(tonePin, toneFrequency);
-  }
-  else {
-    noTone(tonePin);
+
+  unsigned long timTimer = millis();
+  if ((timTimer - lastDispTim) >= 1000) {
+    lastDispTim = timTimer;
+    Disp();
+    SetupStatus();
   }
 }
 
@@ -145,9 +187,12 @@ char coilRead(unsigned int address) {
     // mb2hal: ???
   switch (address) {
     case 0:
-      return digitalRead(ledPin);
+        // not sure why LCNC needs to know LCNC's status, but there you go...
+      return gblStatLcnc;
     case 1:
-      return toneActive;
+      return gblToolChange;
+    case 2:
+        return gblToolPrepare;
   }
 }
 
@@ -163,11 +208,15 @@ boolean coilWrite(unsigned int address, boolean value) {
     */
   switch (address) {
     case 0:
-      digitalWrite(ledPin, value);
+        // LinuxCNC machine on atatus
+      lcncMachineOn(value);
       break;
     case 1:
-      toneActive = value;
+      lcncChangeTool(value);
       break;
+    case 2:
+       gblToolPrepare = value;
+       break;
   }
   return true;
 }
@@ -181,11 +230,28 @@ char discreteInputRead(unsigned int address) {
     iocontrol.0.tool-prepared
     (Bit, In) Should be driven TRUE when a tool prepare is completed.
     */
-  return !digitalRead(buttonPins[address]);
+    switch (address) {
+    case 0:
+        return gblToolIsChanged;
+        break;
+    case 1:
+        return gblToolIsPrepared;
+        break;
+    }
 }
 
 long holdingRegisterRead(unsigned int address) {
     // mb2hal: fnct_03_read_holding_registers
+  switch (address) {
+    case 0:
+      return dutyCycle;
+    case 1:
+      return toneFrequency;
+  }
+}
+
+boolean holdingRegisterWrite(word address, word value) {
+    // mb2hal: fnct_16_write_multiple_registers
     /*
     iocontrol.0.tool-number
     (s32, Out) Current tool number.
@@ -198,25 +264,22 @@ long holdingRegisterRead(unsigned int address) {
     */
   switch (address) {
     case 0:
-      return dutyCycle;
-    case 1:
-      return toneFrequency;
-  }
-}
+        if (value != gblToolNumber) {
+            gblToolNumber = value;
+            gblToolIsChanged = false;
+        }
 
-boolean holdingRegisterWrite(word address, word value) {
-    // mb2hal: fnct_16_write_multiple_registers
-  switch (address) {
-    case 0:
-      dutyCycle = constrain(value, 0, 255);
-      analogWrite(pwmPin, dutyCycle);
       break;
     case 1:
-      toneFrequency = 0;
-      if (value >= 31) {
-        toneFrequency = value;
-      }
+        if (value != gblToolPrepNumber) {
+            gblToolPrepNumber = value;
+            gblToolIsPrepared = false;
+        }
+
       break;
+    case 2:
+        gblToolPrepPocket = value;
+        break;
   }
   return true;
 }
