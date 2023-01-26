12d42cc7f691f777bf4ceff79b6ecf514edb6ab7
Travis Farmer <travis.farmer@outlook.com>
2023-01-26 17:23:01 -0500

    really didn't need MQTT...

--- modbus_slave_lcnc_changer.ino
+++ modbus_slave_lcnc_changer.ino
@@ -1,4 +1,4 @@
-/********************************************//**
+/**
  * \brief Arduino ModBus project, controlling a automatic tool changer.
  * The master/client port will need to be configured using the following settings:
  * Baud Rate: 9600
@@ -6,23 +6,14 @@
  * Parity: None
  * Stop Bit(s): 1
  * Slave/Server ID: 1
- ***********************************************/
+ */
 
 #include "version.h"
 #include <ModbusRTUSlave.h>
-#include <SPI.h>
-#include <Ethernet.h>
-#include <PubSubClient.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27,40,2);
-
-// Update these with values suitable for your hardware/network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xEE };
-IPAddress server(10, 150, 86, 185);
-IPAddress ip(192, 168, 0, 29);
-IPAddress myDns(192, 168, 0, 1);
 
 const int dePin = 12;
 
@@ -44,57 +35,16 @@ bool oldWatchDog = false;
 char WatchDogStatus = '0';
 
 int macStatus = 0;
-int EthStat = 0;
-
 
 byte buf[bufSize];
 ModbusRTUSlave modbus(Serial1, buf, bufSize, dePin);
 
-/********************************************//**
- * \brief PubSubClient callback
- *
- * \param topic char*
- * \param payload char*
- * \param length unsigned int
- * \return void
- *
- ***********************************************/
-void callback(char* topic, char* payload, unsigned int length) {
-  //Serial.print(topic);
-  //Serial.print(":");
-  for (int i=0; i< length; i++) {
-    //Serial.print(payload[i]);
-  }
-  //Serial.print(":");
-  //Serial.println(length);
-}
-
-EthernetClient eClient;
-PubSubClient client(eClient);
-
-long lastReconnectAttempt = 0;
-
-/********************************************//**
- * \brief Auto Reconnect
- *
- * \return boolean
- *
- ***********************************************/
-boolean reconnect() {
-  if (client.connect("arduinoClient")) {
-    Disp();
-    EthStat = 0;
-
-  }
-  return client.connected();
-}
-
-/********************************************//**
+/**
  * \brief Display status on a LCD and PubSubClient
  *
  * \return void
  *
- ***********************************************/
+ */
 void Disp() {
     char buffer[40];
     lcd.setCursor(0,0);
@@ -102,47 +52,21 @@ void Disp() {
     lcd.print(buffer);
     lcd.setCursor(0,1);
     char bufferB[40];
-
     sprintf(bufferB, "IP: %s", Ethernet.localIP());
     lcd.print(bufferB);
 
-    char sz[32];
-    sprintf(sz, "%d", macStatus);
-    client.publish("lcnc/macstatus",sz);
+
 }
 
-/********************************************//**
+/**
  * \brief Arduino Setup
  *
  * \return void
  *
- ***********************************************/
+ */
 void setup() {
   lcd.init();                      // initialize the lcd
   lcd.backlight();
-
-  client.setServer(server, 1883);
-  client.setCallback(callback);
- //Serial.println("Initialize Ethernet with DHCP:");
-  if (Ethernet.begin(mac) == 0) {
-    //Serial.println("Failed to configure Ethernet using DHCP");
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      //Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      EthStat = 1;
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-      //Serial.println("Ethernet cable is not connected.");
-      EthStat = 2;
-    }
-    // try to congifure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip, myDns);
-  } else {
-    //Serial.print("  DHCP assigned IP ");
-    //Serial.println(Ethernet.localIP());
-  }
-  delay(1500);
-  lastReconnectAttempt = 0;
 
   Serial1.begin(baud);
   Serial2.begin(115200);
@@ -153,12 +77,12 @@ void setup() {
   modbus.configureInputRegisters(numInputRegisters, inputRegisterRead);
 }
 
-/********************************************//**
+/**
  * \brief Arduino Loop
  *
  * \return void
  *
- ***********************************************/
+ */
 void loop() {
   modbus.poll();
 
@@ -169,20 +93,6 @@ void loop() {
 
   }
 
-  if (!client.connected()) {
-    long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      // Attempt to reconnect
-      if (reconnect()) {
-        lastReconnectAttempt = 0;
-      }
-    }
-  } else {
-    // Client connected
-
-    client.loop();
-  }
 
   if (oldWatchDog != feedWatchDog && (timTimer - lastWatchDog) < 1000) {
     // watchdog not starving
@@ -198,13 +108,13 @@ void loop() {
 
 
 
-/********************************************//**
+/**
  * \brief ModBus Coil Read handle
  *
  * \param address unsigned int
  * \return char
  *
- ***********************************************/
+ */
 char coilRead(unsigned int address) {
     // mb2hal: fnct_01_read_coils
     switch(address) {
@@ -214,14 +124,14 @@ char coilRead(unsigned int address) {
     }
 }
 
-/********************************************//**
+/**
  * \brief ModBus Coil Write Handle
  *
  * \param address unsigned int
  * \param value boolean
  * \return boolean
  *
- ***********************************************/
+ */
 boolean coilWrite(unsigned int address, boolean value) {
     // mb2hal: fnct_05_write_single_coil
 
@@ -233,25 +143,25 @@ boolean coilWrite(unsigned int address, boolean value) {
   return true;
 }
 
-/********************************************//**
+/**
  * \brief ModBus Discrete Input Read Handle
  *
  * \param address unsigned int
  * \return char
  *
- ***********************************************/
+ */
 char discreteInputRead(unsigned int address) {
     // mb2hal: fnct_02_read_discrete_inputs
 
 }
 
-/********************************************//**
+/**
  * \brief ModBus Holding Register Read Handle
  *
  * \param address unsigned int
  * \return long
  *
- ***********************************************/
+ */
 long holdingRegisterRead(unsigned int address) {
     // mb2hal: fnct_03_read_holding_registers
     switch(address) {
@@ -262,14 +172,14 @@ long holdingRegisterRead(unsigned int address) {
     }
 }
 
-/********************************************//**
+/**
  * \brief ModBus Holding Register Write Handle
  *
  * \param address word
  * \param value word
  * \return boolean
  *
- ***********************************************/
+ */
 boolean holdingRegisterWrite(word address, word value) {
     // mb2hal: fnct_16_write_multiple_registers
 
@@ -284,13 +194,13 @@ boolean holdingRegisterWrite(word address, word value) {
   return true;
 }
 
-/********************************************//**
+/**
  * \brief ModBus Input Register Read Handle
  *
  * \param address word
  * \return long
  *
- ***********************************************/
+ */
 long inputRegisterRead(word address) {
     // mb2hal: fnct_04_read_input_registers
   return analogRead(address);
