e284d9e97fb3ba40d312a787d969d89e88bd0786
Travis Farmer <travis.farmer@outlook.com>
2023-02-13 05:13:53 -0500

    re-doing

--- modbus_slave_lcnc.ino
+++ modbus_slave_lcnc.ino
@@ -10,10 +10,6 @@
 
 #include "version.h"
 #include <ModbusRTUSlave.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27,40,2);
 
 const int dePin = 12;
 
@@ -21,42 +17,17 @@ const byte id = 1;
 const unsigned long baud = 9600;
 const unsigned int bufSize = 256;
 
-const unsigned int numCoils = 11;
+const unsigned int numCoils = 10;
 const unsigned int numDiscreteInputs = 1;
-const unsigned int numHoldingRegisters = 2;
+const unsigned int numHoldingRegisters = 10;
 const unsigned int numInputRegisters = 16;
 
 unsigned long lastDispTim = 0UL;
 unsigned long lastWatchDog = 0UL;
 
-bool feedWatchDog = false;
-bool oldWatchDog = false;
-
-char WatchDogStatus = '0';
-
-int macStatus = 0;
 
 byte buf[bufSize];
 ModbusRTUSlave modbus(Serial1, buf, bufSize, dePin);
-
-/**
- * \brief Display status on a LCD
- *
- * \return void
- *
- */
-void Disp() {
-    char buffer[40];
-    lcd.setCursor(0,0);
-    sprintf(buffer, "LCNC: %d", macStatus);
-    lcd.print(buffer);
-    lcd.setCursor(0,1);
-    char bufferB[40];
-    //sprintf(bufferB, "IP: %s", Ethernet.localIP());
-    lcd.print(bufferB);
-
-
-}
 
 /**
  * \brief Arduino Setup
@@ -65,11 +36,7 @@ void Disp() {
  *
  */
 void setup() {
-  lcd.init();                      // initialize the lcd
-  lcd.backlight();
-
   Serial1.begin(baud);
-  Serial2.begin(115200);
   modbus.begin(id, baud);
   modbus.configureCoils(numCoils, coilRead, coilWrite);
   modbus.configureDiscreteInputs(numDiscreteInputs, discreteInputRead);
@@ -77,6 +44,12 @@ void setup() {
   modbus.configureInputRegisters(numInputRegisters, inputRegisterRead);
   for (int i = 22; i <= 31; i++) {
     pinMode(i,INPUT_PULLUP);
+  }
+  for (int j = 32; j <= 41; j++) {
+    pinMode(j,OUTPUT);
+  }
+  for (int k = 2; k <= 11; k++) {
+    pinMode(k,OUTPUT);
   }
 }
 
@@ -89,24 +62,6 @@ void setup() {
 void loop() {
   modbus.poll();
 
-  unsigned long timTimer = millis();
-  if ((timTimer - lastDispTim) >= 1500) {
-    lastDispTim = timTimer;
-    Disp();
-
-  }
-
-
-  if (oldWatchDog != feedWatchDog && (timTimer - lastWatchDog) < 1000) {
-    // watchdog not starving
-    oldWatchDog = feedWatchDog;
-    lastWatchDog = timTimer;
-    WatchDogStatus = '0';
-  } else if ((timTimer - lastWatchDog) >= 1000){
-    // watchdog starving, hasn't eaten in over 1000ms
-    WatchDogStatus = '1';
-    macStatus = 0; // Machine is lost
-  }
 }
 
 
@@ -122,38 +77,35 @@ char coilRead(unsigned int address) {
     // mb2hal: fnct_01_read_coils
     switch(address) {
     case 0:
-        return WatchDogStatus;
-        break;
-    case 1: // BEGIN Tool Pocket Sensors
         return digitalRead(22)? 0:1;
         break;
-    case 2:
+    case 1:
         return digitalRead(23)? 0:1;
         break;
-    case 3:
+    case 2:
         return digitalRead(24)? 0:1;
         break;
-    case 4:
+    case 3:
         return digitalRead(25)? 0:1;
         break;
-    case 5:
+    case 4:
         return digitalRead(26)? 0:1;
         break;
-    case 6:
+    case 5:
         return digitalRead(27)? 0:1;
         break;
-    case 7:
+    case 6:
         return digitalRead(28)? 0:1;
         break;
-    case 8:
+    case 7:
         return digitalRead(29)? 0:1;
         break;
-    case 9:
+    case 8:
         return digitalRead(30)? 0:1;
         break;
-    case 10:
+    case 9:
         return digitalRead(31)? 0:1;
-        break; // END Tool Pocket Sensors
+        break;
 
     }
 }
@@ -171,11 +123,36 @@ boolean coilWrite(unsigned int address, boolean value) {
 
     switch(address) {
     case 0:
-        feedWatchDog = value;
+        digitalWrite(32,value);
         break;
     case 1:
-        // do nothing yet
+        digitalWrite(33,value);
         break;
+    case 2:
+        digitalWrite(34,value);
+        break;
+    case 3:
+        digitalWrite(35,value);
+        break;
+    case 4:
+        digitalWrite(36,value);
+        break;
+    case 5:
+        digitalWrite(37,value);
+        break;
+    case 6:
+        digitalWrite(38,value);
+        break;
+    case 7:
+        digitalWrite(39,value);
+        break;
+    case 8:
+        digitalWrite(40,value);
+        break;
+    case 9:
+        digitalWrite(41,value);
+        break;
+
 
     }
   return true;
@@ -202,12 +179,7 @@ char discreteInputRead(unsigned int address) {
  */
 long holdingRegisterRead(unsigned int address) {
     // mb2hal: fnct_03_read_holding_registers
-    switch(address) {
-    case 0:
-        return gblStatus;
-        break;
 
-    }
 }
 
 /**
@@ -223,11 +195,36 @@ boolean holdingRegisterWrite(word address, word value) {
 
     switch(address) {
     case 0:
-        gblToolIn = value;
+        analogWrite(2,value);
         break;
     case 1:
-        gblToolOut = value;
+        analogWrite(3,value);
         break;
+    case 2:
+        analogWrite(4,value);
+        break;
+    case 3:
+        analogWrite(5,value);
+        break;
+    case 4:
+        analogWrite(6,value);
+        break;
+    case 5:
+        analogWrite(7,value);
+        break;
+    case 6:
+        analogWrite(8,value);
+        break;
+    case 7:
+        analogWrite(9,value);
+        break;
+    case 8:
+        analogWrite(10,value);
+        break;
+    case 9:
+        analogWrite(11,value);
+        break;
+
     }
   return true;
 }
--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "26";
-	static const char MONTH[] = "01";
+	static const char DATE[] = "13";
+	static const char MONTH[] = "02";
 	static const char YEAR[] = "2023";
-	static const char UBUNTU_VERSION_STYLE[] =  "23.01";
+	static const char UBUNTU_VERSION_STYLE[] =  "23.02";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 2;
-	static const long REVISION  = 18;
+	static const long BUILD  = 0;
+	static const long REVISION  = 0;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 1,0,2,18
-	#define RC_FILEVERSION_STRING "1, 0, 2, 18\0"
-	static const char FULLVERSION_STRING [] = "1.0.2.18";
+	#define RC_FILEVERSION 1,0,0,0
+	#define RC_FILEVERSION_STRING "1, 0, 0, 0\0"
+	static const char FULLVERSION_STRING [] = "1.0.0.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 2;
+	static const long BUILD_HISTORY  = 0;
 	
 
 }
